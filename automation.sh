b1dc61aa628d5a532d379c1795688955ee68ea81
Rakesh More <rakeshmore22@gmail.com>
2022-03-13 12:23:51 +0530

    Create automation.sh
    Adding task 2 automation.sh script

---
+++ automation.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#automation.sh
+#BASH Script for:-
+#1. Hosting Web Server
+#2. Archiving Logs
+
+#Declaring variables
+name="Rakesh"
+s3_bucket="upgrad-rakesh"
+
+#Script updates the package information
+echo "APT Update In-progress...!!!"
+sudo apt -y update
+
+#Script ensures that the HTTP Apache server is installed
+if [ "dpkg-query -W apache2 | awk {'print $1'} = "apache2"" ]; then
+    echo "Apache2 is already installed...!!!"
+else
+    echo "Installing Apache2...!!!"
+    sudo apt-get -y install apache2
+fi
+
+#Script ensures that HTTP Apache server is running
+if systemctl is-active apache2 --quiet; then
+    echo "Apache2 is already running...!!!"
+else
+    echo "Starting Apache2 Service...!!!"
+    sudo service apache2 start
+fi
+
+#Script ensures that HTTP Apache service is enabled
+apacheService=`service apache2 status | grep 'apache2.service; enabled' | wc -l`
+if [ $apacheService -eq 1 ]; then
+    echo "Apache2 Service is already enabled...!!!"
+else
+    echo "Enabling Apache2 Service...!!!"
+    sudo systemctl enable apache2
+fi
+
+#Archiving logs to S3
+echo "TAR Apache2 Logs...!!!"
+tarFileName=$name-httpd-logs-$(date '+%m%d%Y-%H%M%S').tar
+cd /var/log/apache2/
+sudo tar -cvf /tmp/$tarFileName *.log
+echo "Copy Apache2 Logs TAR to S3 Bucket...!!!"
+aws s3 cp /tmp/$tarFileName s3://${s3_bucket}/$tarFileName
